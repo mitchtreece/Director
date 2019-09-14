fcf36d38af26c0db2d36af42c9f973de88cff829
Mitch Treece <mitchtreece@me.com>
2019-09-13 21:20:17 -0700

    Moved DI resolver to SceneDirector

--- Director/Source/DI/SceneDirector+DI.swift
+++ Director/Source/DI/SceneDirector+DI.swift
@@ -7,8 +7,21 @@
 
 import Swinject
 
-public extension SceneDirector /* DI */ {
+private struct AssociatedKeys {
+    static var resolver: UInt8 = 0
+}
 
+public extension SceneDirector /* DI */ {
+    
+    internal var resolver: Resolver? {
+        get {
+            return objc_getAssociatedObject(self, &AssociatedKeys.resolver) as? Resolver
+        }
+        set {
+            objc_setAssociatedObject(self, &AssociatedKeys.resolver, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+    
     /**
      Starts the scene director with a dependency resolver.
      
@@ -16,7 +29,7 @@ public extension SceneDirector /* DI */ {
      - Returns: This scene director instance.
      */
     final func start(with resolver: Resolver) -> Self {
-        self.sceneCoordinator._resolver = resolver
+        self.resolver = resolver
         return start()
     }
     
